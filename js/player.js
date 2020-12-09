9d8d4a5843f9e74ab41b3df4011b06e4e8db7375
Andreas Kaufmann <andreas.kaufmann@pirobase-imperia.com>
2020-12-09 09:27:45 +0100

    Added 2 more Songs to Playlist

--- js/player.js
+++ js/player.js
@@ -14,29 +14,36 @@ var app = new Vue({
             title: 'Demo: Halo Daze, Shallow Haze',
             artist: 'Templetone',
             src: "https://www.andidroid.de/andi/gaudio/halo_daze_shallow_haze.mp3"
+        },
+        {
+            title: 'Demo: Phoenix',
+            artist: 'P:lot',
+            src: "https://www.andidroid.de/andi/gaudio/phoenix.mp3"
+        },
+        {
+            title: 'Demo: Sea Knows When',
+            artist: 'Templetone',
+            src: "https://www.andidroid.de/andi/gaudio/sea_knows_when.mp3"
         }
         ],
         audio: new Audio()
     },
     methods: {
         play: function (song) {
-          if (typeof song.src != "undefined") {
             this.current = song;
             this.audio.src = this.current.src;
-          }
-          this.audio.play();
-          
-          this.isPlaying = true;
+            this.audio.play();
+            this.isPlaying = true;
         },
         pause: function () {
-          this.audio.pause();
-          this.isPlaying = false;
+            this.audio.pause();
+            this.isPlaying = false;
         }
       },
       created () {
-        this.current.title = this.sources[this.index].title;
-        this.current.artist = this.sources[this.index].artist;
-        this.audio.src = this.sources[this.index].src;
+            this.current.title = this.sources[this.index].title;
+            this.current.artist = this.sources[this.index].artist;
+            this.audio.src = this.sources[this.index].src;
       },
     template: 
         `
@@ -49,7 +56,7 @@ var app = new Vue({
 
             <div class="row">
                 <section class="audio col-12 p-2">
-                    <h2 class="source-title">Listening to: {{ current.title }} - <span>{{ current.artist }}</span></h2>
+                    <h2 class="source-title">Listening to:  {{ current.artist }} - {{ current.title }}</h2>
                     <div class="controls">
                         <a title="Play" class="play fa fa-play" v-if="!isPlaying" @click="play"></a>
                         <a title="Pause" class="pause fa fa-pause" v-else @click="pause"></a>
@@ -62,7 +69,7 @@ var app = new Vue({
                     <h3>Can listen to:</h3>
                     <ul>
                         <li v-for="source in sources" :key="source.src" @click="play(source)" :class="(source.src == current.src) ? 'source playing' : 'source'">
-                            {{ source.title }} - {{ source.artist }}
+                            {{ source.artist }} - {{ source.title }}
                         </li>
                     </ul>
                 </section>
